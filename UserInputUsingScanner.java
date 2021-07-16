a9425aa80825aeb82cca4e55b2ffdb4a5706d0d4
Ripan2022 <87544629+Ripan2022@users.noreply.github.com>
2021-07-17 00:16:45 +0530

    Create UserInputUsingScanner.java

---
+++ UserInputUsingScanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class UserInputUsingScanner 
+{
+
+	public static void main(String[] args) 
+  {
+		Scanner scobj = new Scanner(System.in);
+
+		System.out.println("Enter First Value : ");
+		int firstUserInput = scobj.nextInt();
+		System.out.println("Your Input : " + firstUserInput);
+		scobj.close();
+
+	}
+
+}
